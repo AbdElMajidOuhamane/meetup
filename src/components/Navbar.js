02616dc075f6c929c6ad3307e711cde2d1761e83
AbdElMajidOuhamane <abdelmajidouhamane01@gmail.com>
2023-07-19 22:22:47 +0100

    opdate things

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,16 @@ import ProfileAvatar from './ProfileAvatar';
 
 const Navbar = () => {
   const [search, setSearch] = useState(false)
+  const [keyword,setKeyword]=useState()
   const navigate =useNavigate()
   const {logout}=useLogout()
   const {user}=useAuthContext();
   const handleClick=()=>{
     logout()
+  }
+  const handleSearch=(e)=>{
+    e.preventDefault(); // Prevent the default form submission behavior
+    navigate(`/home?keyword=${encodeURIComponent(keyword)}`);
   }
   return (
     
@@ -83,8 +88,8 @@ const Navbar = () => {
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Looking for...." required />
-        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Looking for...." required onChange={(e)=>setKeyword(e.target.value)}/>
+        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleSearch}>Search</button>
     </div>
     
     
